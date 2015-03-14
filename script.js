123c821a98bfaa16c949c8eac9e132c4ba6c9add
root <liaokaien@gmail.com>
2015-03-14 11:13:46 -0400

    littlechanges

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ function onLinkedInLoad() {
     // Handle the successful return from the API call
 function onSuccess(data) {
     var groups = [];
+    window.groups = groups;
     for(var i=0;i<data.groupMemberships._total;i++){
 	groups.push(data.groupMemberships.values[i].group);
     }
     for (var i=0;i<groups.length;i++){
-	getTopic(groups[i]);
+        window.thisTime = i;
+	var  d = new Date();
+	console.log(thisTime,d.getTime());
+	setTimeout("getTopic(groups["+i+"])"
+	,1500*i);
+	
+
     }
 }
     // Handle an error response from the API call
@@ -22,20 +29,24 @@ function onSuccess(data) {
     }
 
     // Use the API call wrapper to request the member's basic profile data
-    function getProfileData() {
+function getProfileData() {
+
         IN.API.Raw("/people/~:(group-memberships)").result(onSuccess).error(onError);
     }
 
 function getTopic(group){
+         var d = new Date();
+    console.log("getTopic:",d.getTime(),group.name);
     this.groupName = group.name;      
 	IN.API.Raw("/groups/"+group.id+"/posts:(id,summary,creator,title,creation-timestamp)?count=30")
 	.result(success).error(onError);
      }
 
 function success(data){
-   console.log(groupName);
+    var d = new Date();
+    console.log("Success:",d.getTime(),groupName);
     var posts = data.values;
-   for(var i=0;i<posts.length;i++){
+    for(var i=0;i<posts.length;i++){
        var creator = posts[i].creator.firstName+" ";
        creator += posts[i].creator.lastName;
        var title = posts[i].title;
