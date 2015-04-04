f152c33f32b218c9b4e50588511281dda15e3e97
liaokaien <liaokaien@foxmail.com>
2015-04-04 19:14:50 -0400

    cc

--- javascript/load.js
+++ javascript/load.js
@@ -1,22 +1,25 @@
-var documents = {};
-var user = {};
-user.id = $(location).attr('search').substr(4);
+$(document).ready(function(){
+    var user = {};
+    user.id = $(location).attr('search').substr(4);
 //console.log(user.id);
-init(user.id);
+    init(user.id);
 
-var searchButton = $('nav').find('input[type=\'button\']');
-var searchBox = $('nav').find('input[type=\'search\']');
+    var searchButton = $('nav').find('input[type=\'button\']');
+    var searchBox = $('nav').find('input[type=\'search\']');
 
-var commentButton = $('.operation').find('input[type=\'button\']');
+    var commentButton = $('.operation').find('input[type=\'button\']');
 
-searchButton.click(function() {
-    console.log($(this).prev().attr('placeholder'));
-    search(searchBox.val());
-});
-
-searchBox.keydown(function(event) {
-    if (event.keyCode == 13) {
-        console.log($(this).attr('placeholder'));
+    searchButton.click(function() {
+        console.log($(this).prev().attr('placeholder'));
         search(searchBox.val());
-    }
+    });
+
+    searchBox.keydown(function(event) {
+        if (event.keyCode == 13) {
+            console.log($(this).attr('placeholder'));
+            search(searchBox.val());
+        }
+    });
+
+
 });
